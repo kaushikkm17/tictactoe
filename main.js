6e6bd8341ac1f114f134fc294864f6db8202a34d
kaushikkm17 <98007556+kaushikkm17@users.noreply.github.com>
2022-07-19 22:55:15 -0400

    set up board

---
+++ main.js
@@ -0,0 +1,6 @@
+//let board = [[X,X,O],[O,X,O],[O,O,X]];
+
+boardSquares = document.querySelectorAll('.gameboard-square')
+
+//get the O/X from board array, update the square in the DOM to either O/X
+
